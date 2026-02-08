0a5e904903fd935018ef0ae141d4c1d3268b079b
OtherApps <linuxnerd123@gmail.com>
2026-02-08 09:24:38 -0800

    Refactor JSON loading and add new data functions
    Refactor loadJson and AddNew functions to use dynamic URLs based on the current month. Added AddNew2 function for additional data submission.

--- public/nathan/functions.js
+++ public/nathan/functions.js
@@ -1,21 +1,58 @@
-const serverURL= "http://192.168.1.147:8080/"; 
+const months = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+const date = new Date();
+
+var checkname= months[date.getMonth()]+"_"+"RoxxieToxxic_vipboard2.json";
+
 
 async function loadJson(){
+		//console.log(serverURL+'vipboard2.json');
+	const testname=serverURL+months[date.getMonth()]+"_"+tsoh+'_vipboard2.json';
+	
 	  try {
-        const response = await fetch(serverURL+'vipboard.json');
+        const response = await fetch(testname);
+		console.log(testname);
         const data = await response.text();
+		//console.log(data);
 		loadViewers(data);
+	  
+    } catch (error) {
+	   console.error('Error fetching data:', error);
+    }
+}
+async function AddNew(id,name,stars,stick)
+{
+const data = {di:id,name:name,stars:stars,stickers:stick,streamer:tsoh};
+	
+	
+	  try {
+        const response = await fetch(serverURL+"vip",
+		{
+		method: 'POST', // Specify the method
+		headers: {
+        'Content-Type': 'application/json',
+					},
+		body: JSON.stringify(data),});
+		
+        const data2 = await response.text();
+		
+		console.log(data2);
+		
 	  
     } catch (error) {
         console.error('Error fetching data:', error);
     }
-
 }
-async function AddNew(id,name,stars,stickers){
+
+async function AddNew2(id,name,stars,stickers){
 	//request.query.name,request.query.stars,request.query.stickers
 	  try {
-        const response = await fetch(serverURL+"vip?di="+id+"&name="+name+"&stars="+stars+"&stickers="+stickers);
-		console.log(serverURL+"vip?di="+id+"&name="+name+"&stars="+stars+"&stickers="+stickers);
+        const response = await fetch(serverURL+"vip?di="+id+"&name="+name+"&stars="+stars+"&stickers="+stickers+"&streamer="+tsoh);
+		
+		
+		console.log(serverURL+"vip?di="+id+"&name="+name+"&stars="+stars+"&stickers="+stickers+"&steamer="+tsoh);
 		
         const data = await response.text();
 		
